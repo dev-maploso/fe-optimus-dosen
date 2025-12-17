206e1d7444f187458cc2118d2029fd9877e79602
Developer Mahad Aly <dev.mahadalyploso@gmail.com>
2025-12-17 19:13:25 +0700

    edit api url lagi

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://optimus.mahadalyploso.com/api/v1",
-   baseURL: "http://localhost:8000/api/v1"
+  baseURL: "https://optimus.mahadalyploso.com/api/v1",
+  //  baseURL: "http://localhost:8000/api/v1"
 });
 
 // Ambil token langsung dari localStorage saja
